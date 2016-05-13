2cd14e47a27155c6f1d7a5920d33ba5bd050bfae
HirokiSato <hsugar.310@toki.waseda.jp>
2016-05-13 18:52:43 +0900

    add solution of prime faster ver.

---
+++ primes/cpp-fast/main.cpp
@@ -0,0 +1,44 @@
+/* Solution for Primes */
+
+#include <iostream>
+using namespace std;
+
+#define MAX 1000
+
+// sieve of Eratosthenes
+void make_list(bool *arr, int sz){
+  for(int i=0; i<=sz; i++) arr[i] = true;
+  arr[0] = arr[1] = false;
+
+  for(int i=2; i<=sz/2; i++){
+    if(arr[i]){
+      for(int j=i*2; j<=sz; j+=i){
+	arr[j] = false;
+      }
+    }
+  }
+  return;
+}
+
+int main(){
+  // initialize prime list
+  bool is_prime[MAX+1];
+  make_list(is_prime, MAX);
+
+  int n;
+  while(cin >> n, n!=0){
+    bool flag = false;
+    for(int i=2; i*i<=n; i++){
+      if(is_prime[i] && n%i==0 && is_prime[n/i]){
+	flag = true;
+	break;
+      }
+    }
+    if( flag ){
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }  
+  return 0;
+}
